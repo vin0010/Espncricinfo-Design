c000159926e14fa45a615b8eac8af33f0ac1bf0c
Vinoth G <vin10catchme@gmail.com>
2018-12-27 12:55:18 +0530

    Added statistics classes

---
+++ src/com/codex/ec/statistics/BattingStatistics.java
@@ -0,0 +1,45 @@
+package com.codex.ec.statistics;
+
+public class BattingStatistics implements Statistics {
+	private long totalRuns;
+	private int numberOfMatches;
+	private int numberOfWins;
+	private int numberOfTies;
+	private int numberOfDraws;
+	private int numberOfNoResults;
+	private float winningPercentage;
+
+	public long getTotalRuns() {
+		return totalRuns;
+	}
+
+	@Override
+	public int getNumberOfMacthes() {
+		return numberOfMatches;
+	}
+
+	@Override
+	public int getNumberOfWins() {
+		return numberOfWins;
+	}
+
+	@Override
+	public int getNumberOfTies() {
+		return numberOfTies;
+	}
+
+	@Override
+	public int getNumberOfDraws() {
+		return numberOfDraws;
+	}
+
+	@Override
+	public int getNumberOfNoResults() {
+		return numberOfNoResults;
+	}
+
+	@Override
+	public float getWinningPercentage() {
+		return winningPercentage;
+	}
+}
